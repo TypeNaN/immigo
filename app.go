af6f38ecabde32600e274716b980df0d9abf1053
TypeNaN <krajaymail@gmail.com>
2022-08-17 16:05:36 +0700

    http service

--- app.go
+++ app.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func simpleRes(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
+	response.Write([]byte(`{"message":"Hello world"}`))
+}
 
 func main() {
 	fmt.Println("Hello world!")
+	router := mux.NewRouter()
+	router.HandleFunc("/", simpleRes).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
