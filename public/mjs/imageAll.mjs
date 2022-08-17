f234b747119b774675851e861c216310d91ca157
TypeNaN <krajaymail@gmail.com>
2022-08-18 06:35:55 +0700

    Front end Remove image

--- public/mjs/imageAll.mjs
+++ public/mjs/imageAll.mjs
@@ -39,13 +39,33 @@ export default class {
       imfile.textContent = value.file
       imdetail.appendChild(imfile)
 
+      const x = document.createElement('div')
+      x.setAttribute('class', 'delete-upload-image')
+      x.innerText = 'X'
+      im.appendChild(x)
+      x.onclick = async (e) => {
+        imcontainer.className = 'imcontainer remove'
+        await this.waitfor(1500)
+        imcontainer.parentNode.removeChild(imcontainer)
+
+        fetch('/api/image/remove', {
+          method: 'POST',
+          headers: { 'Authorization': 'Bearer ' + obj.user.token },
+          body: JSON.stringify({ id: imcontainer.id })
+        }).then(async (res) => {
+          const data = JSON.parse(await res.text())
+          if (res.status == 200) {
+            if (data['refresh'] && data['refresh']['token']) return obj.setUserAuth(data.refresh)
+          }
+        }).catch((e) => console.error(e))
+      }
+
       const edit = (e) => {
         if (e.key == 'Enter' || e.key == 'Escape') {
           if (e.key == 'Escape') if (e.target['tempData']) e.target.textContent = e.target['tempData']
           if (e.key == 'Enter') {
             e.target['tempData'] = e.target.textContent
-
-            console.log(obj.user.token);
+            
             fetch('/api/image/edit', {
               method: 'POST',
               headers: { 'Authorization': 'Bearer ' + obj.user.token },
@@ -63,8 +83,6 @@ export default class {
                   return
                 }
               }
-            }).then((data) => {
-              console.log('images', data)
             }).catch((e) => console.error(e))
           }
           e.target.blur()
@@ -78,6 +96,14 @@ export default class {
       imddesc.onfocus = (e) => e.target['tempData'] = e.target.childNodes[0].textContent
       imddesc.onkeydown = edit
     }
+  }
+
+  waitfor = (millisecond) => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve();
+      }, millisecond);
+    })
   }
 }
 
