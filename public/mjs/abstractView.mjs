aed100d54428fecb0589bc2d4eab5f7ba47df91e
TypeNaN <krajaymail@gmail.com>
2022-08-18 00:15:59 +0700

    Refresh Token เมื่อมีการใช้งาน

--- public/mjs/abstractView.mjs
+++ public/mjs/abstractView.mjs
@@ -15,5 +15,21 @@ export default class {
     if (userAuth == undefined) window.location.href = '/signin'
     else if (userAuth.token == null) window.location.href = '/signin'
     else if (userAuth.exprire - (Math.floor(Date.now()) / 1000) < 0) window.location.href = '/signin'
+    else this.getRefreshToken(userAuth)
+  }
+
+  getRefreshToken = (userAuth) => {
+    if (userAuth['token']) {
+      fetch('/api/user/refresh', {
+        method: 'GET',
+        headers: { 'Authorization': 'Bearer ' + userAuth.token }
+      }).then(async (res) => {
+        if (res.status == 200) {
+          const data = JSON.parse(await res.text())
+          if (data.token) return sessionStorage.setItem('userAuth', JSON.stringify(data))
+        }
+        console.error(e)
+      }).catch((e) => console.error(e))
+    }
   }
 }
